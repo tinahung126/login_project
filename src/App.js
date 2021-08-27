32506b63576dde88fe6661a0658bf09dc75a1af7
Tina <tina126hung@gmail.com>
2021-08-27 09:41:22 +0800

    Refactor and format code style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css'
 import React from 'react'
-
 import { useForm } from './useForm'
 
 function App () {
@@ -16,21 +15,32 @@ function App () {
       return errors
     },
     onSubmit: (values) => console.table(values)
-
   })
   return (
     <div className="App">
       <header className="App-header">
-        {values.onsubmited && <div className="w-25 alert alert-success fs-5 p-2" role="alert">Success!</div>}
-        <input className=" form-control w-25" name="account" onChange={handleChange}
-          value={values.account} placeholder="Account"/>
-          {errors.account && (<div className="text-danger">{errors.account}</div>)}
-        <input className="form-control w-25 mt-3" name="password" onChange={handleChange}
-          value={values.password} placeholder="password" />
-          {errors.password && (<div className="text-danger">{errors.password}</div>)}
+        {values.onsubmited && <div className="alert alert-success fs-5 p-2 w-25"
+            role="alert">Success!</div>}
+        <input className=" form-control w-25"
+              name="account"
+              onChange={handleChange}
+              value={values.account}
+              placeholder="Account" />
+        {errors.account && (<div className="text-danger">{errors.account}</div>)}
+        <input className="form-control w-25 mt-3"
+              name="password"
+              onChange={handleChange}
+              value={values.password}
+              placeholder="password" />
+        {errors.password && (<div className="text-danger">{errors.password}</div>)}
         <label className="form-control-sm form-check-label mb-2">
-          <input className=" form-check-input me-2" type="checkbox" name="rememberMe" onChange={handleChange} checked={values.rememberMe} />Remember Me</label>
-        <button className="btn btn-primary" onClick={handleSubmit}>Login</button>
+          <input className=" form-check-input me-2"
+                type="checkbox"
+                name="rememberMe"
+                onChange={handleChange}
+                checked={values.rememberMe} />Remember Me</label>
+        <button className="btn btn-primary"
+                onClick={handleSubmit}>Login</button>
       </header>
     </div>
   )
